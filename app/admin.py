936a0a292bf5b9dec0ee9bc8752235ed41e2b3b5
Lyle Hanson <lyle2.0@gmail.com>
2010-06-14 15:22:46 -0500

    Fleshing out models and added basic fixture

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,10 @@
-from tomcookery.app.models import Recipe
+from tomcookery.app.models import *
 from django.contrib import admin
 
 admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Duration)
+admin.site.register(Photo)
+admin.site.register(Author)
+admin.site.register(Nutrition)
+admin.site.register(Tag)
